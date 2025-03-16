c12771cee99f8d54cf3d800580d17b04975e0cc3
“zll1019” <xsh1633605415@163.com>
2025-03-17 00:08:31 +0800

    去掉无用代码

--- main.c
+++ main.c
@@ -5,8 +5,7 @@ static void module_init()
     log_init("/var/log/storage/output.log", 0, 0, LOG_LEVEL_DEBUG);
 }
 
-int main() {        // 程序入口函数
-    printf("Hello, World!\n");  // 输出字符串并换行
+int main() {
 
     module_init();
     
@@ -16,5 +15,5 @@ int main() {        // 程序入口函数
     LOG_ERROR("Failed to connect to database");
     LOG_CRITICAL("System overheating!");
 
-    return 0;       // 返回0表示程序正常结束
+    return 0;
 }
